08c77bd046fa0526c952b668a344561f4250ffc6
Nicolas Manrique <nickmasu.16@gmail.com>
2021-06-19 13:54:25 +0200

    adad fragment to list devices

--- IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/MainActivity.java
+++ IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/MainActivity.java
@@ -21,140 +21,21 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private BluetoothDevice mDevice;
     private String TAG = "MainActivity";
-    private String DEVICE_NAME = "Adafruit Bluefruit LE";
-
-    private BluetoothAdapter mBluetoothAdapter;
-    private TextView mTvDeviceInfo;
-    private TextView mTvTemperature;
-    private SwitchCompat mScConnection;
-    private Handler mHandler;
-    private IntentFilter mIntentFilter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(AmbientInfoService.ACTION_CONNECTION_STATUS);
-        mIntentFilter.addAction(AmbientInfoService.ACTION_AMBIENT_INFO);
-
-        mHandler = new Handler(); // Initialize the Handler from the Main Thread
-
-        mTvDeviceInfo = (TextView) findViewById(R.id.tvDeviceInfo);
-        mTvTemperature = (TextView) findViewById(R.id.tvTemperature);
-        mScConnection = (SwitchCompat) findViewById(R.id.scConnection);
-
-        mScConnection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    // startService();
-                    startMockupService();
-                else
-                    stopService();
-            }
-        });
-
-        mDevice = finDevice();
-        mScConnection.setChecked(AmbientInfoService.isRunning);
-        // mScConnection.setEnabled(mDevice != null); // allowed using a real device
-
-        if (mDevice == null) {
-            mTvDeviceInfo.setText("Device \'" + DEVICE_NAME + "\' Not Found");
-            Log.d(TAG, "DEVICE NOT FOUND");
-        } else {
-            mTvDeviceInfo.setText("Device \'" + DEVICE_NAME + "\' Found)");
-            Log.d(TAG, "DEVICE FOUND : " + mDevice.getAddress());
-        }
-    }
-
-    private BluetoothDevice finDevice() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            mTvDeviceInfo.setText("Not Bluetooth supported");
-            return null;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, SelectDeviceFragment.class, null)
+                    .commit();
         }
 
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 1);
-        }
-
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        for (BluetoothDevice device : pairedDevices) {
-            Log.d(TAG, "DEVICE NAME: " + device.getName());
-            //String deviceName = "Galaxy Buds+ (FDF8)";
-            if (device.getName().equals(DEVICE_NAME))
-                return device;
-        }
-
-        return null;
     }
 
-    public void startMockupService() {
-        if (AmbientInfoService.isRunning)
-            return;
-        mScConnection.setEnabled(false);
-        Intent serviceIntent = new Intent(this, AmbientInfoService.class);
-        serviceIntent.putExtra("deviceAddress", "FAKE");
-        ContextCompat.startForegroundService(this, serviceIntent);
-    }
-
-    public void startService() {
-        if (AmbientInfoService.isRunning)
-            return;
-
-        if (mDevice == null)
-            return;
-
-        mScConnection.setEnabled(false);
-        Intent serviceIntent = new Intent(this, AmbientInfoService.class);
-
-        String address = mDevice.getAddress();
-
-        serviceIntent.putExtra("deviceAddress", address);
-        ContextCompat.startForegroundService(this, serviceIntent);
-    }
-
-    public void stopService() {
-        if (!AmbientInfoService.isRunning)
-            return;
-        Intent serviceIntent = new Intent(this, AmbientInfoService.class);
-        stopService(serviceIntent);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        registerReceiver(mReceiver, mIntentFilter);
-    }
-
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            mScConnection.setEnabled(true);
-            if (intent.getAction().equals(AmbientInfoService.ACTION_CONNECTION_STATUS)) {
-                mTvDeviceInfo.setText(intent.getStringExtra("message") + "\n\n");
-                if (!intent.getBooleanExtra("connected", false)) {
-                    stopService();
-                    mScConnection.setChecked(false);
-                }
-            } else if (intent.getAction().equals(AmbientInfoService.ACTION_AMBIENT_INFO)) {
-                mTvTemperature.setText(intent.getFloatExtra("temperature", 0) + "\n\n");
-
-            }
-        }
-    };
-
-    @Override
-    protected void onPause() {
-        unregisterReceiver(mReceiver);
-        super.onPause();
-    }
 
 }
