d82ee14690bede395d8080ae55abe3099c96216a
Nicolas Manrique <nickmasu.16@gmail.com>
2021-06-23 22:38:44 +0200

    cusotmize toolbars

--- IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/MainActivity.java
+++ IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mContext = this;
         mTvBackground = findViewById(R.id.tvBackground);
--- IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/SettingsActivity.java
+++ IoTFridge/app/src/main/java/manrique/nicolas/iotfridge/SettingsActivity.java
@@ -10,6 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         getSupportFragmentManager()
                 .beginTransaction()
